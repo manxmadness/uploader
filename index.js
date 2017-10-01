b76b870a42b6a11d71f1db0ecfed76c5451a23d6
“Kelly <kelly@kellyprudente.com>
2017-09-30 23:16:08 -0400

    i did it

--- index.js
+++ index.js
@@ -11,18 +11,24 @@
  */
 const express = require('express');
 const aws = require('aws-sdk');
+const THREE = require('three');
 
 /*
  * Set-up and run the Express app.
  */
 const app = express();
+const dataFile = require('./lib/untitled-scene.json')
+
 app.set('views', './views');
-app.use(express.static('./public'));
+app.use("/lib", express.static(__dirname + "/lib"));
+// console.log(__dirname + "/lib")
+// app.use(express.static('./public'));
 // app.use(express.static('./lib'));
 // app.use(express.static('./assets'));
 app.engine('html', require('ejs').renderFile);
 app.listen(process.env.PORT || 3000);
 console.log(process.env.PORT)
+// console.log(THREE)
 
 /*
  * Configure the AWS region of the target bucket.
@@ -39,7 +45,16 @@ const S3_BUCKET = process.env.S3_BUCKET;
  * Respond to GET requests to /account.
  * Upon request, render the 'account.html' web page in views/ directory.
  */
-app.get('/account', (req, res) => res.render('account.html'));
+app.get('/account', (req, res) => res.render('account.html'))
+
+app.get('/render', (req, res) => {
+    res.render('three.html')
+})
+// {
+  // console.log(req.query.img)
+  // res.send('<div>hi<div/>')
+  // res.json({img:req.query.img})
+// })
 /*
  * Respond to GET requests to /sign-s3.
  * Upon request, return JSON containing the temporarily-signed S3 request and
@@ -69,13 +84,4 @@ app.get('/sign-s3', (req, res) => {
     res.write(JSON.stringify(returnData));
     res.end();
   });
-});
-
-/*
- * Respond to POST requests to /submit_form.
- * This function needs to be completed to handle the information in
- * a way that suits your application.
- */
-app.post('/save-details', (req, res) => {
-  // TODO: Read POSTed form data and do something useful
 });
